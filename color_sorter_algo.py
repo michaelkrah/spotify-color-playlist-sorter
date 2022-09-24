7d120b605e1cc0896943742f921b606c23367b9c
Michael <mickra@bu.edu>
2022-09-24 15:18:33 -0400

    color_sort improvements

--- color_sorter_algo.py
+++ color_sorter_algo.py
@@ -1,5 +1,7 @@
 import colorsys
 import glob
+import math
+
 import requests
 
 import numpy as np
@@ -36,8 +38,9 @@ def centroid_histogram(clt):
 
 
 def pixel_list_to_hsv(pixel_list):
-    lis = [[pixel_list[0][0], 0], [pixel_list[1][0], 0], [pixel_list[1][0], 0]]
-    for i in range(3):
+    lis = []
+    for i in range(len(pixel_list)):
+        lis.append(list(pixel_list[i]))
         lis[i][1] = colorsys.rgb_to_hsv(pixel_list[i][1][0] / 255, pixel_list[i][1][1] / 255, pixel_list[i][1][2] / 255)
         lis[i][1] = tuple([x * 100 for x in lis[i][1]])
     return lis
@@ -46,35 +49,51 @@ def pixel_list_to_hsv(pixel_list):
 def find_hue(color):
     hue = color[0][1][0]
     for c in color:
-        if c[1][2] > 15 and c[1][1] > 6:
+        if c[1][2] > 16 and c[1][1] > 6:
             hue = c[1][0]
             return hue
     return 1000
+
+
+def find_hue_lum(color, step):
+    for col in color:
+        if col[1][2] > 20 and col[1][1] > 6:
+
+            r, g, b = colorsys.hsv_to_rgb(col[1][0]/100, col[1][1]/100, col[1][2]/100)
+            lum = math.sqrt(.241 * r + .691 * g + .068 * b )
+
+            h2 = int(col[1][0] * step)
+            lum2 = int(lum * step)
+            v2 = int(col[1][2] * step)
+
+            return (h2, lum2, v2)
+
+    return (1000, 1000, 1000)
 
 
 def generate_list(album_stack):
     edited_stack = []
     for album in album_stack:
         col = album[1]
-        color = find_hue(col)
-        edited_stack.append([color, album])
-    print(edited_stack)
+        color = find_hue_lum(col, 8)
+        edited_stack.append([color, album[0]])
     edited_stack.sort()
+    print(edited_stack)
     return edited_stack
 
 
-def main(tracks):
-    stack = []
-
-    for file in glob.glob("albums/*.jpg"):
-        e = dom_colors(file)
-        g = pixel_list_to_hsv(e)
-        stack.append([file, g])
-
-    h = generate_list(stack)
-
-    for album in h:
-        print(album)
+# def main(tracks):
+#     stack = []
+#
+#     for file in glob.glob("albums/*.jpg"):
+#         e = dom_colors(file)
+#         g = pixel_list_to_hsv(e)
+#         stack.append([file, g])
+#
+#     h = generate_list(stack)
+#
+#     for album in h:
+#         print(album)
 
 
 def color_sort_HSV(tracks):
@@ -86,8 +105,13 @@ def color_sort_HSV(tracks):
         except IndexError:
             print("No URL found")
             continue
+        print(URL)
         file = requests.get(URL, stream=True).raw
-        dom_col = dom_colors(file)
+        try:
+            dom_col = dom_colors(file)
+        except Exception:
+            print("Unable to find colors")
+            continue
         print(track.name, dom_col)
         dom_col_hsv = pixel_list_to_hsv(dom_col)
         stack.append([track, dom_col_hsv])
@@ -96,8 +120,6 @@ def color_sort_HSV(tracks):
 
     stack_2 = []
     for item in sorted_list:
-        stack_2.append(item[1][0])
-
-    print(stack_2)
+        stack_2.append(item[1])
 
     return stack_2
